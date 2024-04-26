9cfed9d4b2b6f0f14e5e0244ff49872bf7eb20a2
ud1-t <udittripathi0101@gmail.com>
2024-04-26 14:11:16 +0530

    Dashboard

--- server/model/User.js
+++ server/model/User.js
@@ -11,6 +11,6 @@ const UserSchema = new Schema({
         type: String,
         required: true,
     },
-}, { timestamps: true })
+})
 
 module.exports = mongoose.model('User', UserSchema)
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,8 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../model/Post.js')
 const User = require('../model/User.js')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const adminLayout = '../views/layouts/admin'
+const jwtSecret = process.env.JWT_SECRET
+
+/*
+/ 
+/ Check Login
+/ 
+*/
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token
+    
+    if(!token)  return res.status(401).json({ message: 'Unauthorised' })
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret)
+        req.userId = decoded.userId
+        next()
+    } catch (error) {
+        res.status(401).json({ message: 'Unauthorised' })
+    }
+}
+
+
 /*
 / GET /
 / Admin - login
@@ -23,17 +47,58 @@ router.get('/admin', async (req, res) => {
 
 /*
 / POST /
+/ Admin - Register
+*/
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        try {
+            const user = await User.create({ username, password: hashedPassword })
+            res.status(201).json({ message: 'User created', user })
+        } catch (error) {
+            if(error.code === 11000)
+                res.status(409).json({ message: 'User already in use' })
+            res.status(500).json({ message: 'Internal server error' })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+/*
+/ POST /
 / Admin - Check Login
 */
 router.post('/admin', async (req, res) => {
     try {
         const { username, password } = req.body
 
+        const user = await User.findOne({ username })
 
-        res.send('admin');
+        if(!user)   return res.status(401).json({ message: 'Invalid credentials '})
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordValid)    return res.status(401).json({ message: 'Invalid credentials '})
+
+        const token = jwt.sign({ userId: user._id }, jwtSecret)
+        res.cookie('token', token, { httpOnly: true })
+        res.redirect('/dashboard');
+
     } catch (error) {
         console.log(error)
     }
 });
 
 module.exports = router
+
+/*
+/ GET /
+/ Admin - Dashboard
+*/
+router.get('/dashboard', authMiddleware, async (req, res) => {
+
+    res.render('admin/dashboard')
+})
